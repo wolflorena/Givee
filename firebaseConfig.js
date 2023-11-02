c6b9532ea2688e1c32c34909f59bdf67bf97e6be
wolflorena <wolflorenaani@gmail.com>
2023-11-02 22:09:05 +0200

    Created project

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh4e7r-F87Yy5bbWVmG_jRJfL8dPabl2I",
+  authDomain: "givee-e172f.firebaseapp.com",
+  projectId: "givee-e172f",
+  storageBucket: "givee-e172f.appspot.com",
+  messagingSenderId: "542455215546",
+  appId: "1:542455215546:web:446d94843da46af97dd983",
+};
+
+const app = initializeApp(firebaseConfig);
